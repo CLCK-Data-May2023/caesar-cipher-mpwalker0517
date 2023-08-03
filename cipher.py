d24baa0b1fab0dac03046a3da78927470cd8cee2
Morgan Walker <mpwalker517@gmail.com>
2023-08-03 12:45:53 -0400

    caesar_cipher

--- cipher.py
+++ cipher.py
@@ -1,1 +1,25 @@
 # add your code here
+shift = input("Please enter the number of places to shift: ")
+shift = int(shift)
+
+if shift <= 25:
+    ""
+else:
+    print("You need to enter a number between 0 and 25!")
+    
+message = input("Please enter a sentence: ").lower()
+
+abc = "abcdefghijklmnopqrstuvwxyz"
+cipher = ' '
+
+for char in message:
+    if char in abc:
+        index = abc.find(char)
+        index = index + shift
+        if index > 25:
+            index -= 26
+        char = abc[index]
+    cipher += char
+
+
+print("The encrypted senctence is:", cipher) 
