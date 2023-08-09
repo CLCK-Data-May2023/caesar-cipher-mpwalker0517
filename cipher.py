046be15ee9eea928ab80b611be607b7a243620a5
Morgan Walker <mpwalker517@gmail.com>
2023-08-08 22:24:47 -0400

    caesar_final_push

--- cipher.py
+++ cipher.py
@@ -1,13 +1,15 @@
-# add your code here
+# right shift 5
 shift = 5
-
 shift = int(shift)
 
+#Receive message from input
 message = input("Please enter a sentence: ").lower()
 
-abc = "abcdefghijklmnopqrstuvwxyz"
-cipher = ' '
 
+abc = "abcdefghijklmnopqrstuvwxyz"
+encrypted_sentence = " "
+
+#Encrypt sentence
 for char in message:
     if char in abc:
         index = abc.find(char)
@@ -15,7 +17,7 @@ for char in message:
         if index > 25:
             index -= 26
         char = abc[index]
-    cipher += char
+    encrypted_sentence += char
 
 
-print("The encrypted sentence is: ",cipher)
+print('The encrypted sentence is: '+encrypted_sentence)
