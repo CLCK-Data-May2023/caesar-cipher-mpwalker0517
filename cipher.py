36fa8cd08c7d536a6fdc4fe5d4ec8d31bc07571a
Morgan Walker <mpwalker517@gmail.com>
2023-08-08 21:46:31 -0400

    final_update_again

--- cipher.py
+++ cipher.py
@@ -1,17 +1,5 @@
 # add your code here
-shift = input("Please enter the number of places to shift: ")
-while not shift.isdecimal():
-    print("You need to enter a number between 1 and 25")
-    break
-
-
-
-shift = int(shift)
-
-if shift <= 25:
-    ""
-else:
-    print("You need to enter a number between 0 and 25!")
+shift = 5
 
 message = input("Please enter a sentence: ").lower()
 
